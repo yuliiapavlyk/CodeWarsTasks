7c77d4ac58e3f58dfca4c7c15efb4b85673dc637
Yulia Pavlyk <pavlyk.yuliia@gmail.com>
2018-05-24 09:24:05 +0300

    Added task 'Removing elements'

---
+++ Removing elements/linqTasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace linqTasks
+{
+    class Program
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+            arr = arr.Where((value, index) => index % 2 == 0).ToArray();
+
+            return arr;
+
+        }
+        static void Main(string[] args)
+        {
+
+            object[] my_list = { "Keep", "Remove", "Keep", "Remove", "Keep" };
+            object[] arr = RemoveEveryOther(my_list);
+            foreach (object i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }        
+    }
+}
